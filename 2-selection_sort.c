822fc7689f5212c7a16a9fdc0b107bf4de4340fa
ericpo1sh <ericpo1sh@gmail.com>
2023-08-14 14:26:41 -0700

    Added print function into script

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -16,13 +16,17 @@ void selection_sort(int *array, size_t size)
 			for (j = i + 1; j < size; j++)
 			{
 				if (array[j] < array[min])
+				{
 					min = j;
+					print_array(array, size);
+
 			}
 			if (min != i)
 			{
 				temp = array[min];
 				array[min] = array[i];
 				array[i] = temp;
+				print_array(array, size);
 			}
 		}
 	}
