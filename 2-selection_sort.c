fdd3c723104616e4ab2293ac465c860beb9c983f
ericpo1sh <ericpo1sh@gmail.com>
2023-08-14 14:19:00 -0700

    Selection sort completed

---
+++ 2-selection_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+/**
+ * selection_sort - selection sorts an array of integers
+ * @array: array of integers
+ * @size: size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min, temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < n; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			temp = array[min];
+			array[min] = array[i];
+			array[i] = temp;
+		}
+	}
+}
