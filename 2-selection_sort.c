75c8fb4868d06c2a1dcfb7ff850bc9a7437e101b
ericpo1sh <ericpo1sh@gmail.com>
2023-08-14 14:21:00 -0700

    Fixed typo issue

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,7 +11,7 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
-		for (j = i + 1; j < n; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min])
 				min = j;
