b5f08451605dd17cd239053562e80a5ae1891936
ericpo1sh <ericpo1sh@gmail.com>
2023-08-14 20:57:24 -0700

    Insetion sorting completed

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,10 +1,32 @@
 #include "sort.h"
 /**
- *
- *
- *
+ * insertion_sort_list - insertion sorts the LIST of elements
+ * @list: the list that holds the integers
  */
 void insertion_sort_list(listint_t **list)
 {
-	void(list);
+	listint_t *crnt, *temp;
+
+	if (!list || !(*list) || !(*list)->next)
+		return;
+
+	for (crnt = *list; temp; crnt = crnt->next)
+	{
+		for (; crnt->next && crnt->n > crnt->next->n; print_list(*list))
+		{
+			temp = crnt->next;
+			crnt->next = temp->next;
+			temp->prev = crnt->prev;
+			if (crnt->prev)
+				crnt->prev->next = temp;
+			if (temp->next)
+				temp->next->prev = crnt;
+			crnt->prev = temp;
+			temp->next = crnt;
+			if (temp->prev)
+				crnt = temp->prev;
+			else
+				*list = temp;
+		}
+	}
 }
